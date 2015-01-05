e388d091166f57ce64564204038ded502b8a3a01
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-01-04 22:23:39 -0200

    session test

--- www/index.py
+++ www/index.py
@@ -1,6 +1,8 @@
 <%
 
 import html
+import sessionManager
+
 
 html.initHTML(self)
 html.beginHead(self)
@@ -8,6 +10,11 @@ html.endHead(self)
 html.beginContent(self)
 html.addTitle(self, textStyle.italic("IT WORKS"))
 
+print __GET__['haha'][0]
+
+
+print sessionManager.get(self, 'haha')
+sessionManager.set(self, 'haha', __GET__['haha'][0])
 html.beginForm(self, "form2")
 html.formAddInput(self, "text")
 html.formAddSubmit(self, "manda")
