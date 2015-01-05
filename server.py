4eed5199bd7126b0d65e4aac46c1c0ed8396e075
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-01-05 13:14:17 -0200

    status, url fix

--- server.py
+++ server.py
@@ -11,7 +11,7 @@ import threading
 from BaseHTTPServer import HTTPServer
 from requestHandler import requestHandler
 import multithreadSupport
-
+import statusCheck
 
 execfile('checkFolders.py')
 
@@ -20,9 +20,10 @@ execfile('checkFolders.py')
 if __name__ == '__main__':
     if config.__ENABLE_MULTITHREADING__ == False:
         server = HTTPServer((config.__LISTEN_ADDRESS__, config.__INTERNAL_PORT__), requestHandler)
-        print "MULTI-THREADING: DISABLED"
+        #print "MULTI-THREADING: DISABLED"
     else:
         server = multithreadSupport.ThreadedHTTPServer((config.__LISTEN_ADDRESS__, config.__INTERNAL_PORT__), requestHandler)
-        print "MULTI-THREADING: ENABLED"
+        #print "MULTI-THREADING: ENABLED"
+    statusCheck.printConfigs()
     print 'Starting server, use <Ctrl-C> to stop'
     server.serve_forever()
