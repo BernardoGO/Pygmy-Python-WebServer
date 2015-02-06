e742cb2b0ed8ed72d7b8fd7c54b6c7317c542840
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-06 10:47:59 -0200

    abs path

--- config.py
+++ config.py
@@ -1,16 +1,17 @@
 from server.utils.wstypes import HTMLVer
 import os
 
+path = os.path.abspath
 
-__WWW_DIR__ = os.path.abspath('../elab/www/')
-__SESSIONS_DIR__ = os.path.abspath('sessions/')
+__WWW_DIR__ = path('../elab/www/')
+__SESSIONS_DIR__ = path('sessions/')
 __INTERNAL_PORT__ = 8013
 __LISTEN_ADDRESS__ = ''
 __SESSION_COOKIE_NAME__ = 'sessionId'
 __ALLOWED_DIRS__ = []
 __ALLOWED_DIRS_AND_SUB__ = \
     [
-        os.path.abspath('../elab/www/')
+        path('../elab/www/')
     ]
 __ALLOWED_EXTENSIONS__ = []
 __DENIED_EXTENSIONS__ = ['*.exe']
