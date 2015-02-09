ce13f57c400ae89895473ffe3af0fc442001f939
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-09 16:24:41 -0200

    IP Argument

--- server.py
+++ server.py
@@ -23,21 +23,20 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-v', action="store_true", default=config.__VERBOSE_MODE__)
     parser.add_argument('-p', action="store", default=config.__INTERNAL_PORT__, type=int)
+    parser.add_argument('-ip', action="store", default=config.__LISTEN_ADDRESS__, type=str)
     parser.add_argument('--test', action="store_true", default=False)
     parsed = parser.parse_args()
     if config.__ACCEPT_ARGS__ == True:
         config.__VERBOSE_MODE__ = bool(parsed.v)
         config.__INTERNAL_PORT__ = int(parsed.p)
-
+        config.__LISTEN_ADDRESS__ = str(parsed.ip)
 
     checkFolders.createIfNotExists()
     if config.__ENABLE_MULTITHREADING__ == False:
         server = http((config.__LISTEN_ADDRESS__, config.__INTERNAL_PORT__), requestHandler)
-        #print "MULTI-THREADING: DISABLED"
     else:
         server = multithreadSupport.ThreadedHTTPServer((config.__LISTEN_ADDRESS__, config.__INTERNAL_PORT__),
                                                        requestHandler)
-        #print "MULTI-THREADING: ENABLED"
     statusCheck.printConfigs()
 
     print('Starting server, use <Ctrl-C> to stop')
