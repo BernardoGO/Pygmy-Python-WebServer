d89ff69f4296cdfb55ee7a75883a5fb6d1b972ec
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-08 22:31:51 -0200

    config.conf ready

--- config.py
+++ config.py
@@ -7,24 +7,22 @@ configFilePath = r'config.conf'
 configParser.read(configFilePath)
 
 getc = configParser.get
-
 path = os.path.abspath
 
 __WWW_DIR__ = path(getc('folders', 'wwwFolder'))
 __SESSIONS_DIR__ = path(getc('folders', 'sessionsFolder'))
 __INTERNAL_PORT__ = int(getc('server', 'port'))
-
 addr = getc('server', 'listenAddress')
 if addr != "ALL": __LISTEN_ADDRESS__ = addr
 else: __LISTEN_ADDRESS__ = ""
-__SESSION_COOKIE_NAME__ = 'sessionId'
+__SESSION_COOKIE_NAME__ = getc('server', 'sessionCookieName')
 __ALLOWED_DIRS__ = eval(getc('folders', 'allowedDirs'))
 __ALLOWED_DIRS_AND_SUB__ = [path(x) for x in eval(getc('folders', 'allowedDirsAndSubdirs'))]
 print "ALLOWER: " + str(__ALLOWED_DIRS_AND_SUB__)
 __ALLOWED_EXTENSIONS__ = eval(getc('files', 'allowedExtensions'))
 __DENIED_EXTENSIONS__ = eval(getc('files', 'deniedExtensions'))
-__MAX_POST_LENGTH__ = 2097152
-__INDEX_PAGE__ = 'index.py'
-__ENABLE_MULTITHREADING__ = True
-__HTML_VER__ = HTMLVer.HTML5
-__VERBOSE_MODE__ = True
+__MAX_POST_LENGTH__ = getc('files', 'maxPostLength')
+__INDEX_PAGE__ = getc('files', 'indexPage')
+__ENABLE_MULTITHREADING__ = getc('server', 'enableMultithreading')
+__HTML_VER__ = eval( "HTMLVer." + getc('server', 'htmlVersion') )
+__VERBOSE_MODE__ = getc('server', 'enableVerboseMode')
