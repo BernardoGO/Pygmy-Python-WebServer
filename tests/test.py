f2803fbc7bd692094255a886c2e2b46574b97bf0
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-22 19:13:14 -0300

    Server response OK

--- tests/test.py
+++ tests/test.py
@@ -1,4 +1,10 @@
 __author__ = 'bernardo'
 
+import urllib2
+
 
 raw_input("Start the server on localhost:8000 and press enter.")
+
+
+page = urllib2.urlopen("http://localhost:8000/ping.tst").read()
+print ("Server Response: " + page)
