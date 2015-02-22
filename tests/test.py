53c402bf6db4676535104e76c1160abf537435cf
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-22 19:17:11 -0300

    test ping

--- tests/test.py
+++ tests/test.py
@@ -1,10 +1,14 @@
 __author__ = 'bernardo'
 
 import urllib2
+import time
+from datetime import datetime
 
 
 raw_input("Start the server on localhost:8000 and press enter.")
 
-
+start_time = datetime.now()
 page = urllib2.urlopen("http://localhost:8000/ping.tst").read()
 print ("Server Response: " + page)
+end_time = datetime.now()
+print('Duration: {}'.format(end_time - start_time))
