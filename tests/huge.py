d35303b85fc6bb1350698b5ab5f09655ad5bd7f9
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-22 20:08:49 -0300

    Multithreaded Test

--- tests/huge.py
+++ tests/huge.py
@@ -4,9 +4,15 @@ import urllib2
 import time
 from datetime import datetime
 import argparse
+import thread
 
 PORT = 8000
 ip = "localhost"
+
+
+def call():
+    a = urllib2.urlopen(("http://{0}:{1}/ping.tst").format(str(parsed.ip), str(parsed.p))).read()
+    print a
 
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', action="store", default=PORT, type=int)
@@ -17,6 +23,7 @@ raw_input("Start the server on {0}:{1} and press enter.".format(str(parsed.ip), 
 
 start_time = datetime.now()
 for x in range(0,1000):
-    urllib2.urlopen(("http://{0}:{1}/ping.tst").format(str(parsed.ip), str(parsed.p)))
+    thread.start_new_thread(call, ())
+
 end_time = datetime.now()
 print('Duration: {}'.format((end_time - start_time).total_seconds()* 1000))
