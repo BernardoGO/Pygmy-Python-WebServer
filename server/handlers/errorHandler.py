7395f42fd1837686ca8124a0df4328a63eb99f9d
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-23 12:08:27 -0300

    show errors

--- server/handlers/errorHandler.py
+++ server/handlers/errorHandler.py
@@ -1,1 +1,8 @@
 __author__ = 'BernardoGO'
+
+import config
+from server.utils.bcolors import bcolors
+
+def handle(message, color = bcolors.FAIL):
+    if config.__VERBOSE_MODE__ == True:
+    print ( color+ message + bcolors.ENDC)
