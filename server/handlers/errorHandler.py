a6044314801639793d43014a6de59e703be99c7e
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-02-28 18:00:52 -0300

    Datetime on verbose mode

--- server/handlers/errorHandler.py
+++ server/handlers/errorHandler.py
@@ -2,12 +2,16 @@ __author__ = 'BernardoGO'
 
 import config
 from server.utils.bcolors import bcolors
+import datetime
 
 #level = 0-10
 
 def handle(message, color = bcolors.FAIL, level=5):
+    datew = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
     if config.__VERBOSE_MODE__ == True:
-        print ( color+ message + bcolors.ENDC)
+        print (bcolors.OKBLUE + datew  + ":  "+ bcolors.ENDC \
+               + color+ message + bcolors.ENDC)
 
     if config.__LOG_ERRORS__ == True:
         f = open('errors.log', 'a')
