24cc0d7e13fdc7aa459a8a1dd9c4a5bfad1ee074
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-03-02 17:18:30 -0300

    Removed print on PathManager

--- server/managers/pathManager.py
+++ server/managers/pathManager.py
@@ -19,7 +19,6 @@ def is_directory(file, directory):
 
 def verify_in_directory(file):
     if len(config.__ALLOWED_DIRS_AND_SUB__) < 1:
-        #print (bcolors.WARNING + "THERE IS NO ALLOWED DIRs AND SUBs" + bcolors.ENDC)
         errorHandler.handle("THERE IS NO ALLOWED DIRs AND SUBs", color=bcolors.WARNING, level = 4)
         return False
     for koopa in config.__ALLOWED_DIRS_AND_SUB__:
