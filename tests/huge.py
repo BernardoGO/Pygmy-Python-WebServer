19a1b0bed33a116db07220fcf282f76491f0a4ae
BernardoGO <bernardo.godinho.oliveira@gmail.com>
2015-04-07 02:35:10 -0300

    server

--- tests/huge.py
+++ tests/huge.py
@@ -18,23 +18,29 @@ def call():
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', action="store", default=PORT, type=int)
 parser.add_argument('-ip', action="store", default=ip, type=str)
+parser.add_argument('-f', action="store", default=ip, type=str)
+parser.add_argument('-t', action="store", default=1, type=int)
+
 parsed = parser.parse_args()
 
 threads = []
 
 raw_input("Start the server on {0}:{1} and press enter.".format(str(parsed.ip), str(parsed.p)))
+for x in xrange(0, parsed.t):
+    start_time = datetime.now()
+    for x in range(0,1000):
+        #thread.start_new_thread(call, ())
+        thread = threading.Thread(target=call)
+        thread.start()
 
-start_time = datetime.now()
-for x in range(0,1000):
-    #thread.start_new_thread(call, ())
-    thread = threading.Thread(target=call)
-    thread.start()
+        threads.append(thread)
 
-    threads.append(thread)
+    end_time = datetime.now()
 
-end_time = datetime.now()
+    for thread in threads:
+        thread.join()
 
-for thread in threads:
-    thread.join()
-
-print('Duration: {}'.format((end_time - start_time).total_seconds()* 1000))
+    time = '{}'.format((end_time - start_time).total_seconds()* 1000)
+    with open(parsed.f+".txt", "a") as myfile:
+        myfile.write(time+"\n")
+    print('Duration: '+time)
--- tests/ping.py
+++ tests/ping.py
@@ -11,12 +11,20 @@ ip = "localhost"
 parser = argparse.ArgumentParser()
 parser.add_argument('-p', action="store", default=PORT, type=int)
 parser.add_argument('-ip', action="store", default=ip, type=str)
+parser.add_argument('-f', action="store", default=ip, type=str)
+parser.add_argument('-t', action="store", default=1, type=int)
+
 parsed = parser.parse_args()
 
 raw_input("Start the server on {0}:{1} and press enter.".format(str(parsed.ip), str(parsed.p)))
 
-start_time = datetime.now()
-page = urllib2.urlopen(("http://{0}:{1}/ping.tst").format(str(parsed.ip), str(parsed.p))).read()
-print ("Server Response: " + page)
-end_time = datetime.now()
-print('Duration: {}'.format((end_time - start_time).total_seconds()* 1000))
+for x in xrange(0, parsed.t):
+
+    start_time = datetime.now()
+    page = urllib2.urlopen(("http://{0}:{1}/ping.tst").format(str(parsed.ip), str(parsed.p))).read()
+    print ("Server Response: " + page)
+    end_time = datetime.now()
+    time = '{}'.format((end_time - start_time).total_seconds()* 1000)
+    with open(parsed.f+".txt", "a") as myfile:
+        myfile.write(time+"\n")
+    print('Duration: '+time)
